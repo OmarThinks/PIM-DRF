2e8c933a09f968cddff90f88330340305c587223
Omar Magdy <contact.parmag@gmail.com>
2021-05-21 11:03:20 +0200

    products:models.py:add product serializer

--- pim/products/models.py
+++ pim/products/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from rest_framework import routers, serializers, viewsets
 
 class Product(models.Model):
 	product_code = models.CharField(max_length=50)
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	quantity = models.FloatField()
+
+
+# Serializers define the API representation.
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
