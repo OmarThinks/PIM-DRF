46fd6bbd03be66251a7b642d3844903a8f5dc73a
Omar Magdy <contact.parmag@gmail.com>
2021-05-21 10:31:07 +0200

    products:models.py:created the product model

--- pim/products/models.py
+++ pim/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	product_code = models.CharField(max_length=50)
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	quantity = models.FloatField()
