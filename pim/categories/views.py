f4a2c1311775c323d40deeed5ea1a53f6aaa2c40
Omar Magdy <contact.parmag@gmail.com>
2021-05-21 11:19:57 +0200

    Products:views.py:add:product viewset

--- pim/categories/views.py
+++ pim/categories/views.py
@@ -2,11 +2,7 @@ from rest_framework import viewsets
 from .models import Category, CategorySerializer
 
 
-
-
 # ViewSets define the view behavior.
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-
