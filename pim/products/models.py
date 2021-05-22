b2a15a833e4b2dccc8ed3b5ccc2e30f3ab96989d
Omar Magdy <contact.parmag@gmail.com>
2021-05-22 11:38:19 +0200

    migrated Product:adding categories field

--- pim/products/models.py
+++ pim/products/models.py
@@ -4,15 +4,13 @@ from categories.models import Category
 
 
 
-
 class Product(models.Model):
 	product_code = models.CharField(max_length=50)
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	quantity = models.FloatField()
 	categories = models.ManyToManyField(Category,
-		null=True, blank=True, on_delete = models.SET_NULL, 
-		related_name='products')
+		blank=True, related_name='products')
 
 
 # Serializers define the API representation.
